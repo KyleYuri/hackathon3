996716d047dcf54e6353eb78b0181824866def42
KyleYuri <kylexxzyuri@gmail.com>
2025-09-18 00:44:48 +0800

    name weaver number 1 sample

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -107,7 +107,7 @@ export default async function Home() {
     },
     {
       id: 3,
-      name: "Carmn Flores",
+      name: "Carmen Flores",
       specialty: "Patadyong Design",
       location: "Iloilo City",
       experience: "30 years",
